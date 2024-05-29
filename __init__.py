bfab89a4c9070b17ba9b3f0521db336486fba849
Takafumi Magara <tmagara@nifty.com>
2024-05-30 08:29:14 +0900

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from . import prediction_boost
+
+NODE_CLASS_MAPPINGS = {
+    "PredictionBoost": prediction_boost.PredictionBoost,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "PredictionBoost": "Prediction Boost"
+}
